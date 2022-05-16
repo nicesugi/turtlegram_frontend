1e5c369b78373e5010f937684e9496e750060e0e
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-05-17 00:39:16 +0900

    Feat : 시도중-회원정보 입력 성공시 페이지 이동, 실패시 알림

--- api.js
+++ api.js
@@ -10,26 +10,20 @@ async function handleSignin(){
         body:JSON.stringify(signupData)
     }
     )
+    console.log(response)  
+    
+    // if (response.status ==200) {
+    //     window.location.replace('http://127.0.0.1:5500/turtlegram_frontend/login.html');
+    // } else {
+    //     alert(response.status);
+    // }
+    // function handleSignin(){
+    // if (response == '가입완료') {
+    //     window.location.replace('/turtlegram_frontend/login.html');
+    // }
+    if (response == 'try_again') {
+        console.log('입력해주세요')
+        alert('다시 입력해주세요');
 
-
-//     const response = await fetch('http://172.30.1.17:5005/login' ,{
-//         method:'POST',
-//         body:JSON.stringify(signupData)
-//     }
-//     )
-//     console.log(response)
-// }
-
-
-
-    // redirect 이용해서 새로운페이지
-    // handleSignin 클릭시, 입력한 이메일란에 @이 포함되면 db 저장, 아니라면 메세지.
-    // handleSignin 클릭시, 입력한 이메일이 userDB에 저장되어있다면, 메세지 보내기. 
-
-    if (result == success) {
-        alert("이메일성공");
-        return "redirect:/login";
-    } else {
-        alert(['msg']);
-        return "redirect:/signup";
     }
+}
