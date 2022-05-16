446e3ed7b5d53eebcbfb009bd7217bfa28fe3e15
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-05-16 18:02:01 +0900

    Feat : 시도중-회원정보 입력 성공시 페이지 이동, 실패시 알림

--- api.js
+++ api.js
@@ -5,11 +5,31 @@ async function handleSignin(){
         password : document.getElementById("floatingPassword").value
     }
 
-    const response = await fetch('http://172.30.1.55:5005/signup',{
+    const response = await fetch('http://172.30.1.17:5005/signup' ,{
         method:'POST',
         body:JSON.stringify(signupData)
     }
     )
 
-    console.log(response)
-}
+
+//     const response = await fetch('http://172.30.1.17:5005/login' ,{
+//         method:'POST',
+//         body:JSON.stringify(signupData)
+//     }
+//     )
+//     console.log(response)
+// }
+
+
+
+    // redirect 이용해서 새로운페이지
+    // handleSignin 클릭시, 입력한 이메일란에 @이 포함되면 db 저장, 아니라면 메세지.
+    // handleSignin 클릭시, 입력한 이메일이 userDB에 저장되어있다면, 메세지 보내기. 
+
+    if (result == success) {
+        alert("이메일성공");
+        return "redirect:/login";
+    } else {
+        alert(['msg']);
+        return "redirect:/signup";
+    }
