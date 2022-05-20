cffc84170fe7b7b155401e96ababa6ebc4a6d22e
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-05-20 21:58:41 +0900

    Feat : 버튼하나에 로그인과 로그아웃 기능 추가(if문으로 교차시킴)

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ async function loadArticles(){
     const article_list = document.getElementById("articles")
 
     articles.forEach(article => {   // 리스트에 쓸수 있는 함수 forEach
-        console.log(article)
+        // console.log(article)
         const newArticle = document.createElement("li");
         newArticle.setAttribute("id", article._id)
         newArticle.innerText = article.title
@@ -15,7 +15,26 @@ async function loadArticles(){
     
 }
 
+async function checkLogin(){
+    const name = await getName();
+    console.log(name)
+    const username = document.getElementById("username")
+    const loginoutButton = document.getElementById("loginout")
+    if(name){
+        username.innerText = name
+        loginoutButton.innerText = "로그아웃"
+        loginoutButton.setAttribute("onclick", "logout()")
+    }else{
+        username.innerText = "로그인해주세요"
+        loginoutButton.innerText = "로그인"
+        loginoutButton.setAttribute("onclick", "location.href='/login.html'")
+    }
 
 
+}
+
+
+
+checkLogin();
 loadArticles();
-getName();
+// getName();
