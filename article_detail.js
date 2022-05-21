a01fbedaa91ca1bda5942af31e02a360d1720faa
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-05-21 23:21:09 +0900

    Feat : url을 자바스크립트로 가져오기

--- article_detail.js
+++ article_detail.js
@@ -1,1 +1,7 @@
 console.log('detail페이지 입니당.')
+
+// 유알엘 쿼리값 가져오기
+// urlParams의 ?id 값을 불러올 수 있음
+const urlParams = new URLSearchParams(window.location.search);
+const article_id = urlParams.get('id');
+console.log(article_id)
